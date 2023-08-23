27bee6e5b7cb573c7e70d72374759955f8e42303
Eugene Che <jiykka@gmail.com>
2023-08-23 21:10:26 +0300

    Исправления и третья функция

--- js/function.js
+++ js/function.js
@@ -1,11 +1,9 @@
 // Функция проверки на паллиндром
 
-function palindromeCheck(sameWord) {
+function palindromeCheck(string) {
   sameWord = sameWord.replaceAll(' ', '').toLowerCase();
       const LAST_INDEX = sameWord.length - 1;
       for (let i = 0; i < sameWord.length / 2; i++) {
-           //console.log(sameWord[i]);
-          //  console.log(sameWord[lastIndex - i]);
           if (sameWord[i] !== sameWord[LAST_INDEX - i]) {
               return false;
           }
@@ -15,14 +13,34 @@ function palindromeCheck(sameWord) {
   palindromeCheck('топот твовт топоТ');
 
 
-  // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+  // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 
-   function stringTransform (sameString) {
+   function getNumberFromString (sameString) {
     sameString = sameString.replace(/[^0-9]+/g, '') || Math.abs(sameString);
   return Number(sameString);
    }
-   console.log(stringTransform('tgerg ef -4543.6,4'));
+   console.log(getNumberFromString('tgerg ef -4543.6,4'));
 
+// Функция принимающая три параметра и возвращающая исходную строку, дополненную указанными символами до заданной длины
+
+   const MAKE_NEW_STRING = (string, maxLength, symbol) => {
+    const DIFFERENCE = maxLength - string.length;
+
+    let newString = ''.concat(string);
+    let newSymbol = symbol;
+
+    if (string.length >= maxLength) {
+      return string;
+    } else if (symbol.length >= DIFFERENCE) {
+      newSymbol = symbol.substr(0, DIFFERENCE);
+      newString = newSymbol.concat(string);
+      return newString;
+    }
+    newSymbol = symbol.slice(0, DIFFERENCE % symbol.length) + symbol.repeat(DIFFERENCE / symbol.length);
+    newString = newSymbol.concat(string);
+    return newString;
+  };
+  MAKE_NEW_STRING('q', 4, 'werty');
 
 
   // Функция для проверки длины строки
