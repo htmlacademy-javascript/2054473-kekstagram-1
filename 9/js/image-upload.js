c6dfb4cea7c842cc8b0b854f55de9d01634ccb13
Keks <keks@htmlacademy.ru>
2023-11-27 20:18:40 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/image-upload.js
@@ -0,0 +1,49 @@
+import {isEscapeKey} from './util.js';
+
+const imageUploadForm = document.querySelector('.img-upload__overlay');
+const imageUploadInput = document.querySelector('.img-upload__input');
+const imageUploadCancel = imageUploadForm.querySelector('.img-upload__cancel');
+const preview = imageUploadForm.querySelector('.img-upload__preview');
+
+const openImageUpload = () => {
+  imageUploadForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onPopupEscKeydown);
+};
+
+const closeImageUpload = () => {
+  imageUploadForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  preview.img.src = '';
+  imageUploadInput.value = '';
+};
+
+function onPopupEscKeydown (evt) {
+  if (isEscapeKey(evt.key)) {
+    evt.preventDefault();
+    closeImageUpload();
+  }
+}
+
+const addNewImage = (file) => {
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    const imageUrl = e.target.result;
+    preview.innerHTML = `<img src="${imageUrl}" alt="Preview">`;
+  };
+  reader.readAsDataURL(file);
+};
+
+const effectsPreview = document.querySelectorAll('.effects__preview');
+
+imageUploadInput.addEventListener('change', (evt) => {
+  openImageUpload();
+  addNewImage(evt.target.files[0]);
+  const uploadedPicture = document.querySelector('.img-upload__uploaded-picture');
+  effectsPreview.forEach((effect) => {
+    effect.style.backgroundImage = `url('${uploadedPicture.src}')`;
+  });
+});
+
+imageUploadCancel.addEventListener('click', closeImageUpload);
---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+import './gallery.js';
+import './form-validation.js';
+import './image-upload.js';
+import './slider.js';
+import './scale-control.js';
